59b29dfda7bbdacb1bdd883015e0097f6be60bcc
imoyy <imoyyi@outlook.com>
2024-06-30 23:10:19 +0800

    feat: Update printWidth in .prettierrc to 70
    chore: Update npm dependencies
    
    feat: Add support for lazy loading images and update image domains
    
    fix: Fix styling issue with sticky navbar and long post titles
    
    feat: Update favicon with new image URL
    
    chore: Update blog configuration with new title, author, and description
    
    Update README.md
    
    🔼 Update dependencies
    
    🐛 new window to open RSS to avoid CSR issue
    
    🐛 site title not centered when post title is too long
    
    🐛 react-notion-x not supporting displayMode=true

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import 'prismjs/themes/prism.css'
+// import 'prismjs/themes/prism.css'
+// import 'prismjs/themes/prism-tomorrow.css'\
+import 'prism-themes/themes/prism-atom-dark.css'
 import 'react-notion-x/src/styles.css'
 import 'katex/dist/katex.min.css'
 import App from 'next/app'
@@ -12,23 +14,38 @@ import { prepareDayjs } from '@/lib/dayjs'
 import { ThemeProvider } from '@/lib/theme'
 import Scripts from '@/components/Scripts'
 
-const Ackee = dynamic(() => import('@/components/Ackee'), { ssr: false })
-const Gtag = dynamic(() => import('@/components/Gtag'), { ssr: false })
+const Ackee = dynamic(() => import('@/components/Ackee'), {
+  ssr: false,
+})
+const Gtag = dynamic(() => import('@/components/Gtag'), {
+  ssr: false,
+})
 
-export default function MyApp ({ Component, pageProps, config, locale }) {
+export default function MyApp({
+  Component,
+  pageProps,
+  config,
+  locale,
+}) {
   return (
     <ConfigProvider value={config}>
       <Scripts />
       <LocaleProvider value={locale}>
         <ThemeProvider>
           <>
-            {process.env.VERCEL_ENV === 'production' && config?.analytics?.provider === 'ackee' && (
-              <Ackee
-                ackeeServerUrl={config.analytics.ackeeConfig.dataAckeeServer}
-                ackeeDomainId={config.analytics.ackeeConfig.domainId}
-              />
-            )}
-            {process.env.VERCEL_ENV === 'production' && config?.analytics?.provider === 'ga' && <Gtag />}
+            {process.env.VERCEL_ENV === 'production' &&
+              config?.analytics?.provider === 'ackee' && (
+                <Ackee
+                  ackeeServerUrl={
+                    config.analytics.ackeeConfig.dataAckeeServer
+                  }
+                  ackeeDomainId={
+                    config.analytics.ackeeConfig.domainId
+                  }
+                />
+              )}
+            {process.env.VERCEL_ENV === 'production' &&
+              config?.analytics?.provider === 'ga' && <Gtag />}
             <Component {...pageProps} />
           </>
         </ThemeProvider>
@@ -38,15 +55,18 @@ export default function MyApp ({ Component, pageProps, config, locale }) {
 }
 
 MyApp.getInitialProps = async ctx => {
-  const config = typeof window === 'object'
-    ? await fetch('/api/config').then(res => res.json())
-    : await import('@/lib/server/config').then(module => module.clientConfig)
+  const config =
+    typeof window === 'object'
+      ? await fetch('/api/config').then(res => res.json())
+      : await import('@/lib/server/config').then(
+          module => module.clientConfig,
+        )
 
   prepareDayjs(config.timezone)
 
   return {
     ...App.getInitialProps(ctx),
     config,
-    locale: await loadLocale('basic', config.lang)
+    locale: await loadLocale('basic', config.lang),
   }
 }
